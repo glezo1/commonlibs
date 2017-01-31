7e898336847e1f2484225709cc3d40b817ebc28f
glezo1 <danielgonzalezmarcos@hotmail.com>
2017-01-31 23:53:55 +0100

    Create UnparseableMacException.java

---
+++ com/glezo/mac/UnparseableMacException.java
@@ -0,0 +1,11 @@
+package com.glezo.mac;
+
+public class UnparseableMacException extends Exception 
+{
+	private static final long serialVersionUID = 1L;
+
+	public UnparseableMacException(String msg)
+	{
+		super(msg);
+	}
+}
