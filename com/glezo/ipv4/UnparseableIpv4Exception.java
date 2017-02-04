b39c7b85f1e37d1b7a050b038f2c209410e461ab
glezo1 <danielgonzalezmarcos@hotmail.com>
2017-02-04 12:41:43 +0100

    Create UnparseableIpv4Exception.java

---
+++ com/glezo/ipv4/UnparseableIpv4Exception.java
@@ -0,0 +1,11 @@
+package com.glezo.ipv4;
+
+public class UnparseableIpv4Exception extends Exception
+{
+	private static final long serialVersionUID = 1L;
+
+public UnparseableIpException(String msg)
+	{
+		super(msg);
+	}
+}
