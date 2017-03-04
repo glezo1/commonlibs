2f3d9e1c8333d42856f530fa8b3544c038c32169
glezo1 <danielgonzalezmarcos@hotmail.com>
2017-03-04 17:59:23 +0100

    Create UnparseableIpv4RangeException.java

---
+++ com/glezo/ipv4Range/UnparseableIpv4RangeException.java
@@ -0,0 +1,11 @@
+package com.glezo.ipv4Range;
+
+public class UnparseableIpv4RangeException extends Exception
+{
+	private static final long serialVersionUID = 1L;
+
+	public UnparseableIpv4RangeException(String msg)
+	{
+		super(msg);
+	}
+}
