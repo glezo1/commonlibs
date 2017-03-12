57d5115b201a2b87760541715dc984fbc76fdd95
glezo1 <danielgonzalezmarcos@hotmail.com>
2017-03-12 18:24:28 +0100

    Create UnixFilePermissionsException.java

---
+++ com/glezo/unixFilePermissions/UnixFilePermissionsException.java
@@ -0,0 +1,11 @@
+package com.glezo.unixFilePermissions;
+
+public class UnixFilePermissionsException extends Exception 
+{
+	private static final long serialVersionUID = 1L;
+	public UnixFilePermissionsException(String m)
+	{
+		super(m);
+	}
+
+}
