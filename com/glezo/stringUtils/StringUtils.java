a593bb31c0314bff3c4c83bc6fab022159e1f222
glezo1 <danielgonzalezmarcos@hotmail.com>
2017-03-25 11:59:22 +0100

    added md5(String) and sha1(String)

--- com/glezo/stringUtils/StringUtils.java
+++ com/glezo/stringUtils/StringUtils.java
@@ -72,4 +72,38 @@ public class StringUtils
 		return input.substring(first_significative_char_index,last_significative_char_index);
 	}
 	//-----------------------------------------------------------------------------------------------	
+	public static String				md5(String input) throws NoSuchAlgorithmException 
+	{
+		String result = input;
+		if(input != null) 
+		{
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(input.getBytes());
+			BigInteger hash = new BigInteger(1, md.digest());
+			result = hash.toString(16);
+			while(result.length() < 32) 
+			{
+				result = "0" + result;
+			}
+		}
+		return result;
+	}	
+	//-----------------------------------------------------------------------------------------------------------------------
+	public static String				sha1(String input) throws NoSuchAlgorithmException 
+	{
+		String result = input;
+		if(input != null) 
+		{
+			MessageDigest md = MessageDigest.getInstance("SHA-1");
+			md.update(input.getBytes());
+			BigInteger hash = new BigInteger(1, md.digest());
+			result = hash.toString(16);
+			while(result.length() < 40) 
+			{
+				result = "0" + result;
+			}
+		}
+		return result;
+	}	
+	//-----------------------------------------------------------------------------------------------------------------------
 }
