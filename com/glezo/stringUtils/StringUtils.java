bc42786819dce9044edea0e2e40527ac93307996
glezo1 <danielgonzalezmarcos@hotmail.com>
2017-03-31 23:32:04 +0200

    Update StringUtils.java

--- com/glezo/stringUtils/StringUtils.java
+++ com/glezo/stringUtils/StringUtils.java
@@ -6,6 +6,7 @@ import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 
 public class StringUtils 
 {
