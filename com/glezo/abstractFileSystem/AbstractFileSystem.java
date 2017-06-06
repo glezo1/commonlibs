4cda5b3646b5b95c8800f6151310da07d4b1d0a9
glezo1 <danielgonzalezmarcos@hotmail.com>
2017-06-06 11:22:31 +0200

    Update AbstractFileSystem.java

--- com/glezo/abstractFileSystem/AbstractFileSystem.java
+++ com/glezo/abstractFileSystem/AbstractFileSystem.java
@@ -10,24 +10,32 @@ import com.glezo.generalizedList.GeneralizedListNode;
 	in quite some occasions I have needed an abstract representation of different file-system(ish)
 	e.g.: mtp devices, ptp devices, exploring a remote ext4 after getting a shell, etc. 
 
-	
 */
 
 public class AbstractFileSystem 
 {
 	private GeneralizedList<AbstractFileSystemEntry>	root_node;
-	
+	private int											number_of_files;
+	private int											number_of_directories;
 	
 	public AbstractFileSystem(AbstractFileSystemEntry root)
 	{
 		this.root_node	=	new GeneralizedList<>();
 		GeneralizedListNode<AbstractFileSystemEntry>	root_node_node=new GeneralizedListNode<AbstractFileSystemEntry>(true,root,null,null);
 		this.root_node.set_root_node(root_node_node);
+		this.number_of_directories=0;
+		this.number_of_files=0;
 	}
+	//-----------------------------------------------------------------------------------------------
+	public int	get_number_of_files()		{	return this.number_of_files;		}
+	public int	get_number_of_directories()	{	return this.number_of_directories;	}
 	//-----------------------------------------------------------------------------------------------
 	public void add_entry(AbstractFileSystemEntry e)
 	{
-		String parts[]=e.getPath().split("/");
+		if(e.is_directory())	{	this.number_of_directories++;	}
+		else					{	this.number_of_files++;			}
+		
+		String parts[]=e.get_path().split("/");
 		GeneralizedListNode<AbstractFileSystemEntry> pointer=this.root_node.get_root_node();
 		for(int i=1;i<parts.length;i++)
 		{
@@ -35,7 +43,7 @@ public class AbstractFileSystem
 			if(i+1==parts.length)
 			{
 				//need to add
-				boolean is_dir=e.isDirectory();
+				boolean is_dir=e.is_directory();
 				GeneralizedListNode<AbstractFileSystemEntry> new_node=new GeneralizedListNode<AbstractFileSystemEntry>(is_dir,e,null,pointer);
 				if(pointer.get_son()==null)
 				{
@@ -58,7 +66,7 @@ public class AbstractFileSystem
 				boolean finished=false;
 				while(!finished)
 				{
-					if(pointer.get_data().getName().equals(current_folder))
+					if(pointer.get_data().get_name().equals(current_folder))
 					{
 						finished=true;
 					}
@@ -78,7 +86,8 @@ public class AbstractFileSystem
 		ArrayList<AbstractFileSystemEntry> in_order=this.root_node.in_order();
 		for(int i=0;i<in_order.size();i++)
 		{
-			result+="\n"+in_order.get(i).getPath();
+			result+="\n";
+			result+=in_order.get(i).toString();
 		}
 		//remove first, dummy \n
 		result=result.substring(1);
